e02cc25f41f934484981231d731e7e3db450ba51
Gorbatov Nikita <132291985+Buhanochki@users.noreply.github.com>
2025-01-16 19:45:29 +0300

    Custom User (#1)

---
+++ core/apps/users/admin.py
@@ -0,0 +1,58 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from core.apps.users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "status",
+        "created_at",
+        "updated_at",
+    )
+    fieldsets = (
+        (
+            "Основная информация",
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "last_name",
+                    "first_name",
+                    "surname",
+                    "email",
+                    "status",
+                ),
+            },
+        ),
+        ("О себе", {"classes": ("collapse",), "fields": ("bio",)}),
+        (
+            "Пароль",
+            {
+                "classes": ("wide",),
+                "fields": ("password",),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "last_name",
+                    "first_name",
+                    "surname",
+                    "status",
+                    "email",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
+    ordering = ["id"]
